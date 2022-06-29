04895526a23f95194323cde9b279227901976698
Jennifer Iroh <irohjenny@gmail.com>
2022-06-29 14:26:00 +0100

    added features to populate the html with the data from the database on page refresh

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 // Initialize the Dexie database in our APp
 const db = new Dexie('TinnyShoppingApp')
 // If we are going to change any of the parameters, we alos have to change the version number by increasing by 1
-db.version(1).stores({items: '++id,name,price,isPurchased'})
+db.version(1).stores({ items: '++id,name,price,isPurchased' })
 
 const itemForm = document.getElementById('itemForm')
 const itemsList = document.getElementById('itemsList')
-const itemsTotalPrice = document.getElementsByClassName('itemsTotalPrice')
+const itemsTotalPrice = document.getElementById('itemsTotalPrice')
+
+// How to use items stored in the dexie databas
+const populateItemsList = async () => {
+
+    // convert all items in the database to an array and populate the html using map and template literals
+    const allItems = await db.items.reverse().toArray()
+    itemsList.innerHTML = allItems.map(item => `
+    <div class="item ${item.isPurchased && 'purchased'}">
+        <input 
+        type="checkbox" 
+        class="checkbox"
+        ${item.isPurchased && 'checked'}
+        />
+
+        <div class="itemInfo">
+          <p>${item.itemName}</p>
+          <p> #${item.itemPrice} * ${item.itemQty} </p>
+        </div>
+
+        <button id="deleteItemBtn">X</button>
+      </div>
+    `)
+
+    // Calculate for the total price of items using map and reduce
+    const arrayOfPrices = allItems.map(item => item.itemPrice * item.itemQty)
+    const totalPrice = arrayOfPrices.reduce((a, b) => a + b, 0)
+
+    // populate the total totalprice div html
+    itemsTotalPrice.innerText = 'Total Price: #' + totalPrice
+
+}
+
+// call the populateitemslist function when the window loads up
+window.onload = populateItemsList()
 
 
 // When the submit button is clicked, we want to get the values of our input using an async function and populate the Dexie database
@@ -20,6 +54,7 @@ itemForm.onsubmit = async (event) => {
     // Also, this syntax is like this because of object destructuring
     await db.items.add({ itemName, itemQty, itemPrice })
     
+
     // Clear the form on submit
     itemForm.reset()
 }
